d2444be48111121972ab6350cb2d1454a1d19d59
Ayat <alkayedayat93@gmail.com>
2022-07-04 21:29:49 +0300

    ayat

---
+++ components/ayat.js
@@ -0,0 +1,142 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import emailjs from '@emailjs/browser';
+
+const Contactme = () => {
+  function sendEmail (e){
+      e.preventDefault();
+
+
+      emailjs.sendForm('service_lx0hh2b' ,'template_qpm0brf',e.target,'LRCfH88uLQ2ZuSOzA')
+      
+
+  }
+  return (
+    
+    <>
+      <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet"
+      />
+      <link
+        href="assets/vendor/bootstrap/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
+        rel="stylesheet"
+      />
+      <link href="assets/css/style.css" rel="stylesheet" />
+
+      <Navbar />
+
+      <main id="main">
+        {/* ======= Breadcrumbs ======= */}
+        <div className="breadcrumbs">
+          <div className="container">
+            <h2>Contact Us</h2>
+            <p>
+              Feel free to contact us and give us your feedback. We will be glad
+              to hear from you!
+            </p>
+          </div>
+        </div>
+        {/* End Breadcrumbs */}
+        {/* ======= Contact Section ======= */}
+        <section id="contact" className="contact">
+          <div className="container">
+            <div className="mt-5 row">
+              <div className="col-lg-4">
+                <div className="info">
+                  <div className="address">
+                    <i className="bi bi-geo-alt" />
+                    <h4>Location:</h4>
+                    <p>A108 Adam Street, New York, NY 535022</p>
+                  </div>
+                  <div className="email">
+                    <i className="bi bi-envelope" />
+                    <h4>Email:</h4>
+                    <p>info@example.com</p>
+                  </div>
+                  <div className="phone">
+                    <i className="bi bi-phone" />
+                    <h4>Call:</h4>
+                    <p>+1 5589 55488 55s</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5 col-lg-8 mt-lg-0">
+                <form
+                  action="forms/contact.php"
+                  method="post"
+                  role="form"
+                  className="php-email-form"
+                  onSubmit={sendEmail}
+                >
+                  <div className="row">
+                    <div className="col-md-6 form-group">
+                      <input
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                        required=""
+                      />
+                    </div>
+                    <div className="mt-3 col-md-6 form-group mt-md-0">
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        id="email"
+                        placeholder="Your Email"
+                        required=""
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-3 form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="subject"
+                      id="subject"
+                      placeholder="Subject"
+                      required=""
+                    />
+                  </div>
+                  <div className="mt-3 form-group">
+                    <textarea
+                      className="form-control"
+                      name="message"
+                      rows={5}
+                      placeholder="Message"
+                      required=""
+                      defaultValue={""}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <div className="loading">Loading</div>
+                    <div className="error-message" />
+                    <div className="sent-message">
+                      Your message has been sent. Thank you!
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <button type="submit">Send Message</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* End Contact Section */}
+      </main>
+      {/* End #main */}
+
+      <Footer />
+    </>
+  );
+};
+
+export default Contactme;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import About from "../components/About"
 import Skills from "../components/Skills"
 import Projects from "../components/Projects"
 import Contact from "../components/Contact"
+import Contactme from "../components/ayat"
 
 
 
@@ -23,6 +24,7 @@ export default function Home() {
       <Skills/>
       <Projects/>
       <Contact />
+      <Contactme />
 
     </div>
   )
